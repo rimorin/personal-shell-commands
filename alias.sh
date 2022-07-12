ace12633aa823450eed1b6143cbd3a2091ceebca
Kwan Rimorin John Eric <je_macbook@JE-Macbook.local>
2022-07-12 09:07:45 +0800

    Initial commit

---
+++ alias.sh
@@ -0,0 +1,8 @@
+alias c="clear"
+alias ..='cd ..'
+alias ...='cd ../..'
+alias histg="history | grep"
+#Show which applications are connecting to the network.
+alias listen="lsof -P -i -n" 
+#Get your public IP address and host.
+alias ipinfo="curl ifconfig.me && curl ifconfig.me/host"
---
+++ commands.sh
@@ -0,0 +1,9 @@
+#Mask 3/4 of a string with *
+mask() printf '%s\n' ${(l[$#1][*])1:$#1*3/4}
+
+#Create directory and auto dir into the new directory
+mkcd() { mkdir -p $1; cd $1 }
+
+#Dir into a directory and list its files
+cls() { cd "$1"; ls;}
+
